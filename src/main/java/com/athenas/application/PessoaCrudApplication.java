2f22984826ca6a5f67fb31ac73e2408bd159e77b
Taiwan9 <clownser1994@gmail.com>
2022-06-29 07:29:54 -0300

    Project created

---
+++ src/main/java/com/athenas/application/PessoaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.athenas.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athenas/model/PessoaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athenas.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
